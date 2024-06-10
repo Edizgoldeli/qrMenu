c4e4fb78f6515567098fccd3db58a13935f90799
edizgoldeli <edizgoldeli@hotmail.com>
2024-06-10 10:20:44 +0300

    Menu editing buttons are created

--- PHP/Admin/menuDetails.php
+++ PHP/Admin/menuDetails.php
@@ -3,7 +3,7 @@
     </form>
 <?php
 include_once __DIR__ . "/../dbConnection.php";
-
+$direct = new Url;
 $menuID = $_POST["menuID"]; //Selected menu's ID from previous page
 
 $stmt = $conn->prepare("
@@ -11,8 +11,12 @@ SELECT
     menu.ID as menuID,
     category.categoryName, 
     product.productName, 
+    product.categoryID,
     options.optionName, 
-    options.optionPrice
+    options.optionPrice,
+    options.productID
+    
+
 FROM 
     menu
 JOIN 
@@ -36,27 +40,40 @@ $category = $data['categoryName'];
 $productName = $data['productName'];
 $optionName = $data['optionName'];
 $optionPrice = $data['optionPrice'];
+$categoryID = $data['categoryID'];
+$productID = $data['productID'];
 
 if($category == $categoryCheck){ // Checking if previous products category is same as this product
     if($productName != $productNameCheck){ echo $productName. "<br><br>";} //Checking if previous products name is same as previous one
        echo $optionName . " ". $optionPrice."<br>";
 }else{ 
     echo "<br>".$category. "<br>";
+    ?> 
+<form action="<?php $direct->directory("PHP/Admin","menuDetails"); ?>" method="POST">
+     <input type="hidden" name="categoryID" value="<?= htmlspecialchars($categoryID, ENT_QUOTES, 'UTF-8') ?>" />
+     <input type="hidden" name="menuID" value="<?= htmlspecialchars($menuID, ENT_QUOTES, 'UTF-8') ?>" />
+     <input type="submit" value="Ürün Ekle" />
+
+ </form>
+<?php
     echo $productName;
-    echo "<br>" . $optionName . " " . $optionPrice."<br>";
+    ?> <form action="<?php $direct->directory("PHP/Admin","menuDetails"); ?>" method="POST">
+    <input type="hidden" name="productID" value="<?= htmlspecialchars($productID, ENT_QUOTES, 'UTF-8') ?>" />
+    <input type="hidden" name="categoryID" value="<?= htmlspecialchars($categoryID, ENT_QUOTES, 'UTF-8') ?>" />
+    <input type="hidden" name="menuID" value="<?= htmlspecialchars($menuID, ENT_QUOTES, 'UTF-8') ?>" />
+    <input type="submit" value="Opsiyon Ekle" />
+</form> <?php
+    echo $optionName . " " . $optionPrice."<br>";
+    
 }
    
-    
-
-
 $productNameCheck = $productName;
 $categoryCheck = $category;
 }
-// while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+?> 
+   <form action="<?php $direct->directory("PHP/Admin","menuDetails"); ?>" method="POST">
+        <input type="hidden" name="menuID" value="<?= htmlspecialchars($menuID, ENT_QUOTES, 'UTF-8') ?>" />
+        <input type="submit" value="Yeni Kategori Ekle" />
 
-//     echo "Menu ID: " . $row['menuID'] . "<br>";
-//     echo "Category Name: " . $row['categoryName'] . "<br>";
-//     echo "Product Name: " . $row['productName'] . "<br>";
-//     echo "Option Name: " . $row['optionName'] . "<br>";
-//     echo "Option Price: " . $row['optionPrice'] . "<br><br>";
-// }
+    </form> 
+  
