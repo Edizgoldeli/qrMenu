e9065e3c3e2b255d0a1856e82b9750dbf1e31f39
edizgoldeli <edizgoldeli@hotmail.com>
2024-06-11 09:58:58 +0300

    New functions added

--- CLASS/Sql.class.php
+++ CLASS/Sql.class.php
@@ -9,10 +9,34 @@ class Sql
         $stmt = $conn->prepare("UPDATE menu SET status = 1 WHERE id = :selectedId");
         $stmt->bindParam(':selectedId', $selectedId, PDO::PARAM_INT);
         $stmt->execute();
-    
+
         // Set the rest of the statuses to 0
         $stmt = $conn->prepare("UPDATE menu SET status = 0 WHERE id != :selectedId");
         $stmt->bindParam(':selectedId', $selectedId, PDO::PARAM_INT);
         $stmt->execute();
+    }
+    public function showCategoriesOrder($selectedId)
+    {
+        global $conn;
+        $stmt = $conn->query("SELECT ID, categoryName from category WHERE menuID = $selectedId", PDO::FETCH_ASSOC);
+        foreach ($stmt as $data) {
+
+                $categoryID = $data['ID'];
+                $categoryName = $data['categoryName'];
+                echo $categoryID." ".$categoryName."<br>";
+        }
+        
+    }
+    public function rearrangeCategoriesOrder($selectedId)
+    {
+        global $conn;
+        $stmt = $conn->query("SELECT ID, categoryName from category WHERE menuID = $selectedId", PDO::FETCH_ASSOC);
+        foreach ($stmt as $data) {
+
+                $categoryID = $data['ID'];
+                $categoryName = $data['categoryName'];
+                echo $categoryID." ".$categoryName."<br>";
+        }
+        
     }
 }
